6e2d9ec6c8d904e67b4c38d25d3d7c321bfe07b3
Shiven Shekar <shivenshekar01@gmail.com>
2025-01-19 20:34:31 -0700

    Gui structure

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+pub trait Draw {
+    fn draw(&self);
+}
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
